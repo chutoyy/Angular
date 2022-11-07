7a715ba5aaef37cfa5e874f78c8a7536269443d7
chutoyy <megane.azor@icloud.com>
2022-11-07 12:55:18 +0100

    project

---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+   <h1>
+   Welcome to {{title}}!
+   eoeoeoeoeoo on est lààà
+   oeoeo
+    </h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'Angular';
+}
