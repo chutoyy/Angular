99d46cec9a2d40d7df7f068e3876a41b5aa01cba
chutoyy <megane.azor@icloud.com>
2022-11-07 16:14:06 +0100

    Exo1: afficher students and 1st student

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { STUDENTS } from './mock-student-list';
+import { Student } from './Student';
 
 @Component({
   selector: 'app-root',
   template: `
    <h1>
-   Welcome to {{title}}!
-   eoeoeoeoeoo on est lààà
-   oeoeo
-    </h1>
+      La liste des étudiants
+   </h1>
+    
   `,
   styles: []
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit {
+  StudentList: Student[] = STUDENTS;
+  ngOnInit() : void {
+    console.table(this.StudentList);
+    console.table(this.StudentList[0].name);
+  }
+  
+  selectStudent = (studentName : string) =>{
+    console.table(`Vous avez cliqué sur l'étudiant ${studentName}`)
+  }
+  
 }
