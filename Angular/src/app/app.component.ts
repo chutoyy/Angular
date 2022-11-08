df84fdfbc0a2bf77da7257130807619b06757eeb
chutoyy <megane.azor@icloud.com>
2022-11-08 11:07:54 +0100

    Exo2: *ngIf & *ngFr

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,26 +1,46 @@
 import { Component,  OnInit } from '@angular/core';
 import { STUDENTS } from './mock-student-list';
 import { Student } from './Student';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  template: `
-   <h1>
-      La liste des étudiants
-   </h1>
-    
-  `,
+  templateUrl : './app.component.html',
+  
   styles: []
 })
 export class AppComponent implements OnInit {
   StudentList: Student[] = STUDENTS;
+  studentSelected : Student|undefined;
   ngOnInit() : void {
     console.table(this.StudentList);
     console.table(this.StudentList[0].name);
   }
   
-  selectStudent = (studentName : string) =>{
-    console.table(`Vous avez cliqué sur l'étudiant ${studentName}`)
+  selectStudent = (studentName: Student) =>{
+    console.log(`Vous avez cliqué sur l'étudiant ${studentName}`);
+
   }
+  selectId (event : MouseEvent ){
+    const index:number =+(event.target as HTMLInputElement).value;
+    console.log(`Vous avez cliqué sur l'étudiant ${this.StudentList[index].name}`);
+  }
+  
+  selectOneStudent(studentId :string){
+    const student: Student|undefined = this.StudentList.find(
+      student => student.id == +studentId
+    );
+    if (student) {
+      console.log(`Vous avez cliqué sur l'étudiant ${student.name}`)
+      this.studentSelected = student;
+    }else {
+      console.log("létudiant n'existe pas");
+      this.studentSelected = student;
+    }
+
+  }
+  
+
+ 
   
 }
